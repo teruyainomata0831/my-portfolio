6dd7c319e1cd62c2508244c98d6e2d7755f6a198
teruyainomata0831 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-09 11:53:20 +0900

    お問い合わせ先変更

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,46 +1,49 @@
 <template>
   <div class="contact">
-    <img src="../assets/img.png">
+    <img src="../assets/img.png" />
     <p>
       ビジネスのお話、お仕事依頼、その他...etc
-      <br>
-       お気軽にご連絡ください！
+      <br />お気軽にご連絡ください！
     </p>
-    <a href="mailto:terubot1711@icloud.com"><font-awesome-icon icon="envelope" />terubot1711&#64;icloud.com</a>
-    <a href="https://line.me/ti/p/zgLYzv_eoh"><font-awesome-icon :icon="['fab', 'line']" />LINE ID:terubot1117</a>
+    <a href="mailto:terubot1711@icloud.com">
+      <font-awesome-icon icon="envelope" />inomata1117&#64;eay.jp
+    </a>
+    <a href="https://line.me/ti/p/zgLYzv_eoh">
+      <font-awesome-icon :icon="['fab', 'line']" />LINE ID:teruyainomata
+    </a>
     <Footer />
   </div>
 </template>
 
 <script>
-import Footer from '../components/Footer.vue'
+import Footer from "../components/Footer.vue";
 export default {
   components: {
     Footer
   }
-}
+};
 </script>
 
 <style scoped>
-  img {
-    border-radius: 100px;
-    width: 110px;
-    display: block;
-    margin: auto;
-    margin-top: 50px;
-    margin-bottom: 40px;
-  }
-  p{
-    text-align: center;
-    font-size: 15px;
-  }
-  a{
-    display: flex;
-    justify-content: center;
-    font-size: 15px;
-    color: #1976d2;
-  }
-  a:hover{
-    opacity: 0.6;
-  }
+img {
+  border-radius: 100px;
+  width: 110px;
+  display: block;
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom: 40px;
+}
+p {
+  text-align: center;
+  font-size: 15px;
+}
+a {
+  display: flex;
+  justify-content: center;
+  font-size: 15px;
+  color: #1976d2;
+}
+a:hover {
+  opacity: 0.6;
+}
 </style>
